c020cf899bf104ebec734d85873d776f5d0633e6
BurgerJump <burgerjumpok@gmail.com>
2026-05-31 04:22:34 -0300

    Refactor letter generation and game logic

--- script.js
+++ script.js
@@ -511,7 +511,7 @@ function startTimer(){
       let letterPoints = word.length * 10;
 
       message.innerHTML =
-        "<span style='color:#183f35;font-size:22px'>" +
+        "<span style='color:#983f35;font-size:22px'>" +
         word +
         "</span>" +
         "⭐ " + letterPoints + " PUNTOS<br>" +
